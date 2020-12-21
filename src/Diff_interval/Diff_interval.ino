44356796f7ee09659464823e1b51034cb067efa4
suzakutakumi <suzakutakumi3@gmail.com>
2020-12-21 16:41:52 +0900

    時間関係の追加

---
+++ src/Diff_interval/Diff_interval.ino
@@ -0,0 +1,22 @@
+int RED = 5;
+int BLUE = 13;
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+unsigned long long tR=millis(),tB=millis();
+int INTERVAL_R=500,INTERVAL_B=3000;
+int outR=LOW,outB=LOW;
+void loop() {
+  if(millis()-tR>=INTERVAL_R){
+    tR=millis();
+    outR=!outR;
+    analogWrite(RED,outR*51);
+  }
+  if(millis()-tB>=INTERVAL_B){
+    tB=millis();
+    outB=!outB;
+    digitalWrite(BLUE,outB);
+  }
+}
