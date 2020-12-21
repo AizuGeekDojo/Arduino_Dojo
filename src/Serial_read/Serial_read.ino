69c199433a673cba0a071fd840a55045fbb6b1f1
suzakutakumi <suzakutakumi3@gmail.com>
2020-12-21 16:04:53 +0900

    シリアル入力の追加

---
+++ src/Serial_read/Serial_read.ino
@@ -0,0 +1,16 @@
+int LED = 5;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == 'H') {
+      digitalWrite(LED, HIGH);
+    } else if (c == 'L') {
+      digitalWrite(LED, LOW);
+    }
+  }
+}
